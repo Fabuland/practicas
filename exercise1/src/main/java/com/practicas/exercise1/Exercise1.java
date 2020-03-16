45186853e526e7e41a67a3ee0869107ffe711e64
Carlos <carlos_tropical@hotmail.es>
2020-03-16 17:42:47 +0100

    Cuarto commit

--- exercise1/src/main/java/com/practicas/exercise1/Exercise1.java
+++ exercise1/src/main/java/com/practicas/exercise1/Exercise1.java
@@ -13,12 +13,14 @@ public class Exercise1 {
 		FuncionesEjercicio1 funciones = new FuncionesEjercicio1();
 		
 		System.out.println("Marca y modelo en un intervalo:\n");
-		funciones.marcaModeloIntervalo(array, 24,32);
+		System.out.println(funciones.marcaModeloIntervalo(array, 24,32));
 		System.out.println("\nMarca y modelo de los coches con potencia mayor a X:\n");
-		funciones.marcaModeloPotencia(array, 10, 150);
+		System.out.println(funciones.marcaModeloPotencia(array, 10, 150));
 		System.out.println("\nMarca y modelo de los coches automáticos o manuales:\n");
-		funciones.marcaModeloAutomáticos(array, "automatico");
-
+		System.out.println(funciones.marcaModeloAutomáticos(array, "automatico"));
+		System.out.println("\nMarca y modelo de los coches con traccion trasera o sin ella:\n");
+		System.out.println(funciones.marcaModeloTraccionTrasera(array, true));
+		//funciones.marcaModeloTraccionTrasera(array, true);
 	}
 
 }
--- exercise1/src/main/java/com/practicas/exercise1/FuncionesEjercicio1.java
+++ exercise1/src/main/java/com/practicas/exercise1/FuncionesEjercicio1.java
@@ -10,21 +10,27 @@ public class FuncionesEjercicio1 {
 	 * @param suelo primer coche que busca
 	 * @param techo ultimo coche que busca
 	 */
-	public void marcaModeloIntervalo(JSONArray array, int suelo, int techo) {
+	public JSONArray marcaModeloIntervalo(JSONArray array, int suelo, int techo) {
 
-		int nCoches = suelo - 1;
+		int contador = suelo - 1;
 		// Recorre n veces del coche número "suelo" al "techo" para recoger el modelo y
 		// la marca
 		// de cada uno en orden
 
+		JSONArray arrayReturn = new JSONArray();
 		for (int i = suelo - 1; i < techo; i++) {
-			String modelo = ((JSONObject) array.get(nCoches)).getJSONObject("Identification").getString("ID");
-			String marca = ((JSONObject) array.get(nCoches)).getJSONObject("Identification").getString("Make");
+			JSONObject objetoReturn = new JSONObject();
+			JSONObject jObjeto = array.getJSONObject(contador);
+			objetoReturn.put("Make", jObjeto.getJSONObject("Identification").getString("Make"));
+			objetoReturn.put("ID", jObjeto.getJSONObject("Identification").getString("ID"));
+			arrayReturn.put(objetoReturn);
 
-			nCoches++;
-			System.out.println("Coche " + (nCoches) + ": [Modelo: " + modelo + "], [Marca: " + marca + "]");
+			contador++;
+			
 
 		}
+		
+		return arrayReturn;
 	}
 
 	/**
@@ -33,32 +39,34 @@ public class FuncionesEjercicio1 {
 	 * @param nCoches numero de coches que busca
 	 * @param potencia potencia minima para recoger la marca y el modelo
 	 */
-	public void marcaModeloPotencia(JSONArray array, int nCoches, int potencia) {
+	public JSONArray marcaModeloPotencia(JSONArray array, int nCoches, int potencia) {
 
 		// int que sale del while y a su vez controla la posición en los arrays de
 		// modelo y marca
 		int diezPrimeros = 0;
 		// int para recorrer todos los coches hasta que salga del while
-		int todosCoches = 0;
+		int contador = 0;
+		JSONArray arrayReturn = new JSONArray();
 		while (diezPrimeros < nCoches) {
 
-			int cocheHorsepower = ((JSONObject) array.get(todosCoches)).getJSONObject("Engine Information")
+			int cocheHorsepower = ((JSONObject) array.get(contador)).getJSONObject("Engine Information")
 					.getJSONObject("Engine Statistics").getInt("Horsepower");
 
 			// Si tiene más de "potencia" CV recoge el modelo y marca de esos coches para
 			// guardarlos
 			// en los arrays y los muestra
 			if (cocheHorsepower > potencia) {
-				String modelo = ((JSONObject) array.get(todosCoches)).getJSONObject("Identification").getString("ID");
-				String marca = ((JSONObject) array.get(todosCoches)).getJSONObject("Identification").getString("Make");
-
-				System.out
-						.println("Coche " + (diezPrimeros + 1) + ": [Modelo: " + modelo + "], [Marca: " + marca + "]");
+				JSONObject objetoReturn = new JSONObject();
+				JSONObject jObjeto = array.getJSONObject(contador);
+				objetoReturn.put("Make", jObjeto.getJSONObject("Identification").getString("Make"));
+				objetoReturn.put("ID", jObjeto.getJSONObject("Identification").getString("ID"));
+				arrayReturn.put(objetoReturn);
 				diezPrimeros++;
 			}
-			todosCoches++;
+			contador++;
 
 		}
+		return arrayReturn;
 	}
 	
 	/**
@@ -66,11 +74,12 @@ public class FuncionesEjercicio1 {
 	 * @param array
 	 * @param transmision tipo de transmision que se puede introducir,las dos opciones son "automatico" o "manual"
 	 */
-	public void marcaModeloAutomáticos(JSONArray array, String transmision) {
+	public JSONArray marcaModeloAutomáticos(JSONArray array, String transmision) {
 		
 		int nTotales = array.length();
 		int contador = 0;
 		String transmIntr = "";
+		JSONArray arrayReturn = new JSONArray();
 		//Depende de lo que introduzcas asigna un valor u otro a la variable transmIntr
 		if(transmision.equals("automatico")) {
 			transmIntr = "Automatic transmission";
@@ -85,14 +94,53 @@ public class FuncionesEjercicio1 {
 			String transmCoche = ((JSONObject) array.get(contador)).getJSONObject("Identification").getString("Classification");
 			//Si la transmision del coche es igual a la introducida por el usuario, imprime el modelo y la marca del coche
 			if(transmCoche.equals(transmIntr)) {
-				String modelo = ((JSONObject) array.get(contador)).getJSONObject("Identification").getString("ID");
-				String marca = ((JSONObject) array.get(contador)).getJSONObject("Identification").getString("Make");
-				System.out
-						.println("Coche " + (contador + 1) + ": [Modelo: " + modelo + "], [Marca: " + marca + "]");
+				JSONObject objetoReturn = new JSONObject();
+				JSONObject jObjeto = array.getJSONObject(contador);
+				objetoReturn.put("Make", jObjeto.getJSONObject("Identification").getString("Make"));
+				objetoReturn.put("ID", jObjeto.getJSONObject("Identification").getString("ID"));
+				arrayReturn.put(objetoReturn);
 			}
 			contador++;
 		}
+		return arrayReturn;
 		
 	}
-
-}
+	
+	/**
+	 * 
+	 * @param array
+	 * @param traccion    true o false para definir la traccion
+	 * @return            devuelve el array
+	 */
+	public JSONArray marcaModeloTraccionTrasera(JSONArray array, boolean traccion) {
+			
+			int nTotales = array.length();
+			int contador = 0;
+			String traseraIntr = "Rear-wheel drive";
+			JSONArray arrayReturn = new JSONArray();
+			
+			while(contador < nTotales) {
+				
+				String traccCoche = ((JSONObject) array.get(contador)).getJSONObject("Engine Information").getString("Driveline");
+				//Si la traccion del coche es trasera y coincide con lo que ha introducido el usuario (true), imprime modelo y marca
+				if(traccCoche.equals(traseraIntr) && traccion == true) {
+					JSONObject objetoReturn = new JSONObject();
+					JSONObject jObjeto = array.getJSONObject(contador);
+					objetoReturn.put("Make", jObjeto.getJSONObject("Identification").getString("Make"));
+					objetoReturn.put("ID", jObjeto.getJSONObject("Identification").getString("ID"));
+					arrayReturn.put(objetoReturn);
+				//Si la traccion del coche no es trasera y coincide con lo que ha introducido el usuario (false), imprime modelo y marca
+				}else if(!traccCoche.equals(traseraIntr) && traccion == false) {
+					JSONObject objetoReturn = new JSONObject();
+					JSONObject jObjeto = array.getJSONObject(contador);
+					objetoReturn.put("Make", jObjeto.getJSONObject("Identification").getString("Make"));
+					objetoReturn.put("ID", jObjeto.getJSONObject("Identification").getString("ID"));
+					arrayReturn.put(objetoReturn);
+				}
+				contador++;
+			}
+			return arrayReturn;
+			
+		}
+	
+	}
