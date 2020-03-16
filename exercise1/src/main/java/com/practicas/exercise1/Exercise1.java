219d291a5658928fe5ea9339d9478ff3c456e3ac
Carlos <carlos_tropical@hotmail.es>
2020-03-16 12:46:46 +0100

    Segundo ejercicio

--- exercise1/src/main/java/com/practicas/exercise1/Exercise1.java
+++ exercise1/src/main/java/com/practicas/exercise1/Exercise1.java
@@ -6,14 +6,18 @@ import com.practicas.exercise1.utils.DatabaseJson;
 
 public class Exercise1 {
 
-    public static void main(String[] args){
+	public static void main(String[] args) {
 
-        JSONArray array = DatabaseJson.loadDatabase().getData();
-        //String cadena = ((JSONObject)array.get(0)).getJSONObject("Engine Information").getString("Engine Type");
-        //System.out.println("Engine Type " + cadena);
-        FuncionesEjercicio1 funciones = new FuncionesEjercicio1();
-        funciones.marcaModelo24al32(array);
+		JSONArray array = DatabaseJson.loadDatabase().getData();
+		// String cadena = ((JSONObject)array.get(0)).getJSONObject("Engine
+		// Information").getString("Engine Type");
+		// System.out.println("Engine Type " + cadena);
+		FuncionesEjercicio1 funciones = new FuncionesEjercicio1();
+		System.out.println("Marca y modelo del 24 al 32:\n");
+		funciones.marcaModelo24al32(array, 91,102);
+		System.out.println("\nMarca y modelo de los mayores de 150CV:\n");
+		funciones.marcaModelo150CV(array, 17, 350);
 
-    }
-   
+	}
+
 }
--- exercise1/src/main/java/com/practicas/exercise1/FuncionesEjercicio1.java
+++ exercise1/src/main/java/com/practicas/exercise1/FuncionesEjercicio1.java
@@ -4,20 +4,51 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 public class FuncionesEjercicio1 {
-	
-	public void marcaModelo24al32(JSONArray array) {
 
-    	String[] modelo = new String[9];
-    	String[] marca = new String[9];
-    	int nModelo = 23;
-    	for(int i = 0; i < 9; i++) {
-    		modelo[i] = ((JSONObject)array.get(nModelo)).getJSONObject("Identification").getString("Model Year");
-    		marca[i] = ((JSONObject)array.get(nModelo)).getJSONObject("Identification").getString("Make");
-    		
-    		
-    		System.out.println("Coche " + (nModelo+1) + ": [Modelo: " + modelo[i] + "], [Marca: " + marca[i] + "]");
-    		nModelo++;
-    	}
-    }
+	public void marcaModelo24al32(JSONArray array, int suelo, int techo) {
+
+		int nCoches = suelo-1;
+		// Recorre n veces del coche número "suelo" al "techo" para recoger el modelo y la marca
+		// de cada uno en orden
+		
+		
+		for (int i = suelo-1; i < techo; i++) {
+			String modelo = ((JSONObject) array.get(nCoches)).getJSONObject("Identification").getString("ID");
+			String marca = ((JSONObject) array.get(nCoches)).getJSONObject("Identification").getString("Make");
+
+			nCoches++;
+			System.out.println("Coche " + (nCoches) + ": [Modelo: " + modelo + "], [Marca: " + marca + "]");
+
+		}
+	}
+
+	public void marcaModelo150CV(JSONArray array, int nCoches, int potencia) {
+
+		// int que sale del while y a su vez controla la posición en los arrays de
+		// modelo y marca
+		int diezPrimeros = 0;
+		// int para recorrer todos los coches hasta que salga del while
+		int todosCoches = 0;
+		while (diezPrimeros < nCoches) {
+
+			int cocheHorsepower = ((JSONObject) array.get(todosCoches)).getJSONObject("Engine Information")
+					.getJSONObject("Engine Statistics").getInt("Horsepower");
+
+			// Si tiene más de "potencia" CV recoge el modelo y marca de esos coches para guardarlos
+			// en los arrays y los muestra
+			if (cocheHorsepower > potencia) {
+				String modelo = ((JSONObject) array.get(todosCoches)).getJSONObject("Identification")
+						.getString("ID");
+				String marca = ((JSONObject) array.get(todosCoches)).getJSONObject("Identification")
+						.getString("Make");
+
+				System.out.println("Coche " + (diezPrimeros + 1) + ": [Modelo: " + modelo + "], [Marca: "
+						+ marca + "]");
+				diezPrimeros++;
+			}
+			todosCoches++;
+
+		}
+	}
 
 }
