8678f343f5b34dbf47dcf09a57aea86963293fe9
Carlos <carlos_tropical@hotmail.es>
2020-03-18 11:52:11 +0100

    Comienzo ejercicio 2, funciones 1 y 2

--- exercise2/exercise2-service/src/main/java/com/practicas/service/CarService.java
+++ exercise2/exercise2-service/src/main/java/com/practicas/service/CarService.java
@@ -1,6 +1,8 @@
 package com.practicas.service;
 
 import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -9,58 +11,61 @@ import com.practicas.model.Car;
 import com.practicas.service.data.DatabaseJson;
 
 public class CarService {
-	
-	public List<Car> marcaModeloIntervalo(JSONArray array, int suelo, int techo) {
 
-		int contador = suelo - 1;
-		// Recorre n veces del coche número "suelo" al "techo" para recoger el modelo y
+	/**
+	 * 
+	 * @param listCar
+	 * @param suelo primer coche que busca
+	 * @param techo ultimo coche que busca
+	 * @return
+	 */
+	public static List<Car> marcaModeloIntervalo(List<Car> listCar, int suelo, int techo) {
+
+		if (suelo > techo) {
+			return null;
+		}
+		// Recorre n veces del coche numero "suelo" al "techo" para recoger el modelo y
 		// la marca
 		// de cada uno en orden
 
-		JSONArray arrayReturn = new JSONArray();
-		List<Car> listCar = DatabaseJson.loadDatabase().getDataParsed();
-		for (int i = suelo - 1; i < techo; i++) {
-			JSONObject objetoReturn = new JSONObject();
-			JSONObject jObjeto = array.getJSONObject(contador);
-			objetoReturn.put("make", jObjeto.getJSONObject("Identification").getString("Make"));
-			objetoReturn.put("id", jObjeto.getJSONObject("Identification").getString("ID"));
-			arrayReturn.put(objetoReturn);
+		if (suelo < 0) {
+			suelo = 0;
+		}
 
-			contador++;
-
+		if (techo <= 0 || techo > listCar.size()) {
+			techo = listCar.size();
 		}
 
 		return listCar.subList(suelo, techo);
 	}
-	
-	public List<Car> marcaModeloPotencia(JSONArray array, int nCoches, int potencia) {
 
-		// int que sale del while y a su vez controla la posición en los arrays de
-		// modelo y marca
-		int diezPrimeros = 0;
-		// int para recorrer todos los coches hasta que salga del while
-		int contador = 0;
-		JSONArray arrayReturn = new JSONArray();
-		while (diezPrimeros < nCoches) {
+	/**
+	 * 
+	 * @param listCar
+	 * @param nCoches  numero de coches que busca
+	 * @param potencia potencia minima para recoger la marca y el modelo
+	 * @return
+	 */
+	public static List<Car> marcaModeloPotencia(List<Car> listCar, int nCoches, int potencia) {
 
-			int cocheHorsepower = ((JSONObject) array.get(contador)).getJSONObject("Engine Information")
-					.getJSONObject("Engine Statistics").getInt("Horsepower");
-
-			// Si tiene más de "potencia" CV recoge el modelo y marca de esos coches para
-			// guardarlos
-			// en los arrays y los muestra
-			if (cocheHorsepower > potencia) {
-				JSONObject objetoReturn = new JSONObject();
-				JSONObject jObjeto = array.getJSONObject(contador);
-				objetoReturn.put("make", jObjeto.getJSONObject("Identification").getString("Make"));
-				objetoReturn.put("id", jObjeto.getJSONObject("Identification").getString("ID"));
-				arrayReturn.put(objetoReturn);
-				diezPrimeros++;
-			}
-			contador++;
-
+		if (potencia < 0) {
+			return null;
 		}
-		return arrayReturn;
+
+		Predicate<Car> p = new Predicate<Car>() {
+
+			@Override
+			public boolean test(Car t) {
+
+				return t.getEngineinformation().getEnginestatistics().getHorsepower() > potencia;
+			}
+
+		};
+
+		List<Car> listCarReturn = listCar.parallelStream().filter(p).limit(nCoches).collect(Collectors.toList());
+
+		return listCarReturn;
+
 	}
-	
+
 }
--- exercise2/exercise2-service/src/main/java/com/practicas/service/main/Exercise2.java
+++ exercise2/exercise2-service/src/main/java/com/practicas/service/main/Exercise2.java
@@ -1,7 +1,7 @@
 package com.practicas.service.main;
 
 import java.util.List;
-
+import com.practicas.service.CarService;
 import com.practicas.model.Car;
 import com.practicas.service.data.DatabaseJson;
 
@@ -10,7 +10,12 @@ public class Exercise2 {
 	public static void main(String[] args) {
 		
 		List<Car> listCar = DatabaseJson.loadDatabase().getDataParsed();
-		System.out.println(listCar);
+		//1
+		System.out.println("Marca y modelo en un intervalo:\n");
+		System.out.println(CarService.marcaModeloIntervalo(listCar, 19, 24));
+		// 2
+		System.out.println("\nMarca y modelo de los coches con potencia mayor a X:\n");
+		System.out.println(CarService.marcaModeloPotencia(listCar, 10, 150));
 	}
 
 }
